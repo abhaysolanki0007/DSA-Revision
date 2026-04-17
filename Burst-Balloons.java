326a50cfd29778841e02889a336ebefd1024e3d0
abhaysolanki0007 <abhaysolanki1712@gmail.com>
2026-04-17 11:09:11 +0530

    [312] [Time Beats: 42.07%] [Memory Beats: 35.61%] - LeetPush

--- Burst-Balloons.java
+++ Burst-Balloons.java
@@ -1,32 +1,26 @@
 1class Solution {
 2    public int dp[][];
-3    public int f(int nums[],int i,int j){
-4        int n=nums.length;
+3    public int f(int i,int j,int nums[]){
+4        if(i>j) return 0;
 5        int sum=0;
-6       
-7        if(i>j){
-8            return 0;
+6        if(dp[i][j]!=-1) return dp[i][j];
+7        for(int ind=i;ind<=j;ind++){
+8            sum=Math.max(sum,nums[ind]*nums[i-1]*nums[j+1]+f(i,ind-1,nums)+f(ind+1,j,nums));
 9        }
-10         if(dp[i][j]!=-1) return dp[i][j];
-11        for(int k=i;k<=j;k++){
-12             int left=1;
-13            if(i-1>=0){
-14                left=nums[i-1];
-15            }
-16             int right=1;
-17            if(j+1<n){
-18                right=nums[j+1];
-19            }
-20            sum=Math.max(sum,nums[k]*left*right+f(nums,i,k-1)+f(nums,k+1,j));
-21        }
-22        return dp[i][j]=sum;
-23    }
-24    public int maxCoins(int[] nums) {
-25        int n=nums.length;
-26        dp=new int[n][n];
-27        for(int row[]:dp){
-28            Arrays.fill(row,-1);
-29        }
-30        return f(nums,0,nums.length-1);
-31    }
-32}
+10        return dp[i][j]=sum;
+11    }
+12    public int maxCoins(int[] nums) {
+13        int n=nums.length;
+14        int arr[]=new int[n+2];
+15        dp=new int[n+2][n+2];
+16        for(int row[]:dp){
+17            Arrays.fill(row,-1);
+18        }
+19        arr[0]=1;
+20        arr[n+1]=1;
+21        for(int i=1;i<=n;i++){
+22            arr[i]=nums[i-1];
+23        }
+24        return f(1,n,arr);
+25    }
+26}
